33fc1eceb47c13b8ec34115bfc91127ecd2b2c3d
Dec <decbrks@pm.me>
2026-01-11 00:00:22 +0000

    revise coloured text Colours().red().bold().print("test") ^ still not happy with it

--- oldham_quiz/colours.py
+++ oldham_quiz/colours.py
@@ -4,54 +4,29 @@
 class Colours:
     """ANSI colour codes for terminal output."""
 
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
-    WHITE = '\033[97m'
-
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+    RED = '\033[91m'; GREEN = '\033[92m'; YELLOW = '\033[93m'
+    BLUE = '\033[94m'; MAGENTA = '\033[95m'; CYAN = '\033[96m'
+    WHITE = '\033[97m'; BOLD = '\033[1m'; UNDERLINE = '\033[4m'
     RESET = '\033[0m'
+
+    def __init__(self): self._codes = []
 
     @staticmethod
     def disable():
         """Disable Colours (for non-ANSI terminals)."""
-        Colours.RED = ''
-        Colours.GREEN = ''
-        Colours.YELLOW = ''
-        Colours.BLUE = ''
-        Colours.MAGENTA = ''
-        Colours.CYAN = ''
-        Colours.WHITE = ''
-        Colours.BOLD = ''
-        Colours.UNDERLINE = ''
+        Colours.RED = ''; Colours.GREEN = ''; Colours.YELLOW = ''
+        Colours.BLUE = ''; Colours.MAGENTA = ''; Colours.CYAN = ''
+        Colours.WHITE = ''; Colours.BOLD = ''; Colours.UNDERLINE = ''
         Colours.RESET = ''
 
-    def red_str(this, msg): return f"{this.RED}{msg}{this.RESET}"
-    def green_str(this, msg): return f"{this.GREEN}{msg}{this.RESET}"
-    def yellow_str(this, msg): return f"{this.YELLOW}{msg}{this.RESET}"
-    def blue_str(this, msg): return f"{this.BLUE}{msg}{this.RESET}"
-    def magenta_str(this, msg): return f"{this.MAGENTA}{msg}{this.RESET}"
-    def cyan_str(this, msg): return f"{this.CYAN}{msg}{this.RESET}"
-    def white_str(this, msg): return f"{this.WHITE}{msg}{this.RESET}"
-    def bold_str(this, msg): return f"{this.BOLD}{msg}{this.RESET}"
-    def underline_str(this, msg): return f"{this.UNDERLINE}{msg}{this.RESET}"
+    def red(self): self._codes.append(self.RED); return self
+    def green(self): self._codes.append(self.GREEN); return self
+    def yellow(self): self._codes.append(self.YELLOW); return self
+    def blue(self): self._codes.append(self.BLUE); return self
+    def bold(self): self._codes.append(self.BOLD); return self
+    def underline(self): self._codes.append(self.UNDERLINE); return self
 
-    def red(this, msg): print(this.red_str(this, msg))
-    def green(this, msg): print(this.green_str(this, msg))
-    def yellow(this, msg): print(this.yellow_str(this, msg))
-    def blue(this, msg): print(this.blue_str(this, msg))
-    def magenta(this, msg): print(this.magenta_str(this, msg))
-    def cyan(this, msg): print(this.cyan_str(this, msg))
-    def white(this, msg): print(this.white_str(this, msg))
-    def bold(this, msg): print(this.bold_str(this, msg))
-    def underline(this, msg): print(this.underline_str(this, msg))
+    def format(self, msg): return f"{''.join(self._codes)}{msg}{self.RESET}"
+    def print(self, msg): print(self.format(msg))
 
-
-print(Colours.bold_str(Colours, Colours.red_str(Colours, "test")))
-
-Colours.bold(Colours, Colours.red_str(Colours, "test1"))
+Colours().red().bold().print("test")
