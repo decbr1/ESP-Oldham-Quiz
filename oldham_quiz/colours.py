86f959a67a064f3a7b0d1ddbe49225544330f6f3
Dec <decbrks@pm.me>
2026-01-11 00:06:12 +0000

    revise coloured text with style() print(style("test").red().bold()) ^ still not happy with it

--- oldham_quiz/colours.py
+++ oldham_quiz/colours.py
@@ -1,9 +1,7 @@
 """ANSI colour codes for terminal output."""
 
-
 class Colours:
     """ANSI colour codes for terminal output."""
-
     RED = '\033[91m'; GREEN = '\033[92m'; YELLOW = '\033[93m'
     BLUE = '\033[94m'; MAGENTA = '\033[95m'; CYAN = '\033[96m'
     WHITE = '\033[97m'; BOLD = '\033[1m'; UNDERLINE = '\033[4m'
@@ -23,10 +21,35 @@ class Colours:
     def green(self): self._codes.append(self.GREEN); return self
     def yellow(self): self._codes.append(self.YELLOW); return self
     def blue(self): self._codes.append(self.BLUE); return self
+    def magenta(self): self._codes.append(self.MAGENTA); return self
+    def cyan(self): self._codes.append(self.CYAN); return self
+    def white(self): self._codes.append(self.WHITE); return self
     def bold(self): self._codes.append(self.BOLD); return self
     def underline(self): self._codes.append(self.UNDERLINE); return self
 
     def format(self, msg): return f"{''.join(self._codes)}{msg}{self.RESET}"
     def print(self, msg): print(self.format(msg))
 
-Colours().red().bold().print("test")
+
+class Style:
+    def __init__(self, text):
+        self.text = text
+        self.codes = []
+
+    def red(self): self.codes.append(Colours.RED); return self
+    def green(self): self.codes.append(Colours.GREEN); return self
+    def yellow(self): self.codes.append(Colours.YELLOW); return self
+    def blue(self): self.codes.append(Colours.BLUE); return self
+    def magenta(self): self.codes.append(Colours.MAGENTA); return self
+    def cyan(self): self.codes.append(Colours.CYAN); return self
+    def white(self): self.codes.append(Colours.WHITE); return self
+    def bold(self): self.codes.append(Colours.BOLD); return self
+    def underline(self): self.codes.append(Colours.UNDERLINE); return self
+
+    def __str__(self): return f"{''.join(self.codes)}{self.text}{Colours.RESET}"
+
+def style(msg): return Style(msg)
+
+
+print(style("test").red().bold())
+print(style("success").green())
